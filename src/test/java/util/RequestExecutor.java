83273a48177f361df5d60a661b8abeddbb9c5cc0
James Simpson <james.simpson@securetrading.com>
2019-02-20 18:11:00 +0000

    Updated RequestExecutor to successfully mark build as failed

--- src/test/java/util/RequestExecutor.java
+++ src/test/java/util/RequestExecutor.java
@@ -4,16 +4,27 @@ import static util.PropertiesHandler.getProperty;
 
 import util.enums.PropertyType;
 import util.enums.StoredElement;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.Json;
 
 public final class RequestExecutor {
 
     public static void markTestAsFailed() {
         Runtime runtime = Runtime.getRuntime();
         try {
-            Process process = runtime.exec("curl -u \"" + getProperty(PropertyType.BS_USERNAME) + ":" + getProperty(PropertyType.BS_ACCESS_KEY) + "\" -X PUT -H \"Content-Type: " +
-                    "application/json\" -d \"{\\\"status\\\":\\\"failed\\\", \\\"reason\\\":\\\""
-                    + PicoContainerHelper.getFromContainer(StoredElement.errorMessage) + "\\\"}\" https://api.browserstack.com/automate/sessions/"
-                    + PicoContainerHelper.getFromContainer(StoredElement.sessionId) + ".json");
+            String jsonData = Json.createObjectBuilder()
+                .add("status", "failed")
+                .add("reason", PicoContainerHelper.getFromContainer(StoredElement.errorMessage).toString())
+                .build().toString();
+
+            String cmd = "curl -u '" + getProperty(PropertyType.BS_USERNAME) 
+            + ":" + getProperty(PropertyType.BS_ACCESS_KEY) + 
+            "' -X PUT -H \"Content-Type: " + "application/json\" -d '"+ jsonData 
+            + "' https://api.browserstack.com/automate/sessions/"
+            + PicoContainerHelper.getFromContainer(StoredElement.sessionId) + ".json";
+            
+            Process process = runtime.exec(cmd);
             int resultCode = process.waitFor();
         } catch (Throwable e) {
             e.printStackTrace();
