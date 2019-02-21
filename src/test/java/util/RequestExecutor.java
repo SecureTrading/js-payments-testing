94ed751e9cc12041017485adc7673bc2cfc85b3f
Krzysztof Baranowski <kbaranowski@pgs-soft.com>
2019-02-21 14:37:22 +0100

    temporary resassured version of marking failing tests

--- src/test/java/util/RequestExecutor.java
+++ src/test/java/util/RequestExecutor.java
@@ -1,31 +1,43 @@
 package util;
 
+import static io.restassured.RestAssured.given;
 import static util.PropertiesHandler.getProperty;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import util.enums.PropertyType;
 import util.enums.StoredElement;
+import util.models.ErrorMessage;
+
 import javax.json.Json;
+import java.io.IOException;
+import java.io.InputStream;
 
 public final class RequestExecutor {
 
     public static void markTestAsFailed() {
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            String jsonData = Json.createObjectBuilder()
-                .add("status", "failed")
-                .add("reason", PicoContainerHelper.getFromContainer(StoredElement.errorMessage).toString())
-                .build().toString().replace("\"","\\\"");
+        System.out.println("------------------------------------- rest assured");
+            ErrorMessage errorMessage = new ErrorMessage();
+            errorMessage.setStatus("failed");
+            errorMessage.setReason(PicoContainerHelper.getFromContainer(StoredElement.errorMessage).toString());
 
-            String cmd = "curl -u \"" + getProperty(PropertyType.BS_USERNAME)
-            + ":" + getProperty(PropertyType.BS_ACCESS_KEY) +
-            "\" -X PUT -H \"Content-Type: application/json\" -d \""+ jsonData
-            + "\" https://api.browserstack.com/automate/sessions/"
-            + PicoContainerHelper.getFromContainer(StoredElement.sessionId) + ".json";
+            System.out.println(given()
+//                    .header("Authorization:", "Basic "+getProperty(PropertyType.BS_USERNAME)+":"+getProperty(PropertyType.BS_ACCESS_KEY))
+                    .spec(buildJsonRequestSpecification())
+                    .when()
+                    .body(errorMessage)
+                    .put("https://"+getProperty(PropertyType.BS_USERNAME)+":"+getProperty(PropertyType.BS_ACCESS_KEY)+"@api.browserstack.com/automate/sessions/" + PicoContainerHelper.getFromContainer(StoredElement.sessionId) + ".json").asString());
+        System.out.println("------------------------------------- rest assured");
+    }
 
-            Process process = runtime.exec(cmd);
-            int resultCode = process.waitFor();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+
+    public static RequestSpecification buildJsonRequestSpecification() {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .build();
     }
 }
