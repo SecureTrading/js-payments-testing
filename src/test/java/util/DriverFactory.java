183b540dc73ef38ce1183a50668eb9066c7bd0c2
James Simpson <james.simpson@securetrading.com>
2019-02-27 11:40:12 +0000

    Moved BrowserStackLocal to travis

--- src/test/java/util/DriverFactory.java
+++ src/test/java/util/DriverFactory.java
@@ -69,7 +69,8 @@ abstract class DriverFactory {
         DesiredCapabilities caps = new DesiredCapabilities();
 
         // Browser/device configuration
-        for (String property : new String[]{"os", "os_version", "browser", "browser_version", "resolution", "device", "real_mobile"}) {
+        for (String property : new String[] { "os", "os_version", "browser", "browser_version", "resolution", "device",
+                "real_mobile" }) {
             String value = System.getProperty(property);
             if (value != null) {
                 caps.setCapability(property, value);
@@ -83,12 +84,14 @@ abstract class DriverFactory {
 
         caps.setCapability("project", "JS Payments Interface");
         caps.setCapability("build", LocalDate.now().toString());
-        caps.setCapability("name", PicoContainerHelper.getFromContainer(StoredElement.scenarioName) + " --- " + new Date());
+        caps.setCapability("name",
+                PicoContainerHelper.getFromContainer(StoredElement.scenarioName) + " --- " + new Date());
 
-        if (System.getProperty(PropertyType.LOCAL.toString()) != null && System.getProperty(PropertyType.LOCAL.toString()).equals("true")) {
+        if (System.getProperty(PropertyType.LOCAL.toString()) != null
+                && System.getProperty(PropertyType.LOCAL.toString()).equals("true")) {
             caps.setCapability("browserstack.local", "true");
 
-            local = new Local();
+            // local = new Local();
             Map<String, String> options = new HashMap<String, String>();
             options.put("key", getProperty(PropertyType.BS_ACCESS_KEY));
             options.put("onlyAutomate", "true");
@@ -106,12 +109,16 @@ abstract class DriverFactory {
     private static WebDriver createDriver() {
         if (!getProperty(PropertyType.TARGET).equals("local")) {
             try {
-                driver = new RemoteWebDriver(new URL("https://" + getProperty(PropertyType.BS_USERNAME) + ":" + getProperty(PropertyType.BS_ACCESS_KEY) + "@hub.browserstack.com/wd/hub"), GetRemoteDriverCapabilities());
+                driver = new RemoteWebDriver(
+                        new URL("https://" + getProperty(PropertyType.BS_USERNAME) + ":"
+                                + getProperty(PropertyType.BS_ACCESS_KEY) + "@hub.browserstack.com/wd/hub"),
+                        GetRemoteDriverCapabilities());
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
 
-            PicoContainerHelper.updateInContainer(StoredElement.sessionId, ((RemoteWebDriver) driver).getSessionId().toString());
+            PicoContainerHelper.updateInContainer(StoredElement.sessionId,
+                    ((RemoteWebDriver) driver).getSessionId().toString());
         } else {
             System.setProperty("webdriver.chrome.driver", "./src/binary/chrome/chromedriver.exe");
             driver = new ChromeDriver();
