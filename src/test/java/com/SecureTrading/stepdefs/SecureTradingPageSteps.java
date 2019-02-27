1b5522a6d5f3df2eadff7db64087cf1e954cc266
Krzysztof Baranowski <kbaranowski@pgs-soft.com>
2019-02-27 15:46:02 +0100

    restored response files

--- src/test/java/com/SecureTrading/stepdefs/SecureTradingPageSteps.java
+++ src/test/java/com/SecureTrading/stepdefs/SecureTradingPageSteps.java
@@ -1,13 +1,11 @@
 package com.SecureTrading.stepdefs;
 
-import static io.restassured.RestAssured.given;
 import static util.MocksHandler.stubVersion1;
 import static util.MocksHandler.stubVersion2;
 
 import com.SecureTrading.pageobjects.SecureTradingPage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import io.restassured.http.ContentType;
 import org.junit.Assert;
 import util.PicoContainerHelper;
 import util.SeleniumExecutor;
@@ -51,7 +49,6 @@ public class SecureTradingPageSteps {
         Thread.sleep(1000);
         SeleniumExecutor.getDriver().get("http://localhost:8760/pet");
         SeleniumExecutor.getDriver().navigate().refresh();
-        PicoContainerHelper.updateInContainer(StoredElement.errorMessage, "Wrong endpoint triggered");
         System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% - version 2");
     }
 }
