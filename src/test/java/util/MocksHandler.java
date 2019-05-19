ea2bf3d387c06df6cb6aa102c817f8ff05020966
James Simpson <james.simpson@securetrading.com>
2019-05-19 21:43:30 +0100

    More accurage URLs

--- src/test/java/util/MocksHandler.java
+++ src/test/java/util/MocksHandler.java
@@ -8,34 +8,44 @@ import util.enums.PropertyType;
 
 public class MocksHandler {
 
-    public static void stubSTRequestType(String mockJson, String requestType) {
-        String gatewayUrl = "/jwt/";// TODO add to property
-        MocksHandler.stubUrlOptionsForCors(gatewayUrl);
+    public static void configureForLocalhost() {
         WireMock.configureFor("https", "localhost", Integer.parseInt(getProperty(PropertyType.PORT)));
-        WireMock.stubFor(post(urlEqualTo(gatewayUrl)).withRequestBody(containing(requestType))
-                .willReturn(aResponse().withStatus(200).withHeader("Access-Control-Allow-Origin", "*")
-                        .withHeader("Access-Control-Allow-Headers", "*")
-                        .withHeader("Access-Control-Allow-Methods", "GET, POST").withBodyFile(mockJson)));
+    }
+
+    public static void stubSTRequestType(String mockJson, String requestType) {
+        MocksHandler.stubUrlOptionsForCors(PropertyType.GATEWAY_MOCK_URI);
+        MocksHandler.configureForLocalhost();
+        WireMock.stubFor(
+                post(urlEqualTo(getProperty(PropertyType.GATEWAY_MOCK_URI))).withRequestBody(containing(requestType))
+                        .willReturn(aResponse().withStatus(200).withHeader("Access-Control-Allow-Origin", "*")
+                                .withHeader("Access-Control-Allow-Headers", "Content-Type")
+                                .withHeader("Access-Control-Allow-Methods", "GET, POST").withBodyFile(mockJson)));
     }
 
     public static void stubSTRequestTypeServerError(String requestType) {
-        String gatewayUrl = "/jwt/";// TODO add to property
-        MocksHandler.stubUrlOptionsForCors(gatewayUrl);
-        WireMock.configureFor("https", "localhost", Integer.parseInt(getProperty(PropertyType.PORT)));
-        WireMock.stubFor(post(urlEqualTo(gatewayUrl)).withRequestBody(containing(requestType))
-                .willReturn(aResponse().withStatus(500)));
+        MocksHandler.stubUrlOptionsForCors(PropertyType.GATEWAY_MOCK_URI);
+        MocksHandler.configureForLocalhost();
+        WireMock.stubFor(
+                post(urlEqualTo(getProperty(PropertyType.GATEWAY_MOCK_URI))).withRequestBody(containing(requestType))
+                        .willReturn(aResponse().withStatus(500).withHeader("Access-Control-Allow-Origin", "*")
+                                .withHeader("Access-Control-Allow-Headers", "Content-Type")
+                                .withHeader("Access-Control-Allow-Methods", "GET, POST")));
     }
 
-    public static void stubUrlOptionsForCors(String mockUrl) {
-        WireMock.configureFor("https", "localhost", Integer.parseInt(getProperty(PropertyType.PORT)));
-        WireMock.stubFor(options(urlEqualTo(mockUrl)).willReturn(aResponse().withStatus(200)
-                .withHeader("Access-Control-Allow-Origin", "*").withHeader("Access-Control-Allow-Headers", "*")
-                .withHeader("Access-Control-Allow-Methods", "GET, POST").withBody("")));
+    public static void stubUrlOptionsForCors(PropertyType mockUrl) {
+        MocksHandler.configureForLocalhost();
+        WireMock.stubFor(options(urlEqualTo(getProperty(mockUrl)))
+                .willReturn(aResponse().withStatus(200).withHeader("Access-Control-Allow-Origin", "*")
+                        .withHeader("Access-Control-Allow-Headers", "Content-Type")
+                        .withHeader("Access-Control-Allow-Methods", "GET, POST").withBody("")));
     }
 
     public static void stubPaymentStatus(PropertyType mockUrl, String mockJson) {
-        WireMock.configureFor("https", "localhost", Integer.parseInt(getProperty(PropertyType.PORT)));
-        WireMock.stubFor(
-                get(urlEqualTo(getProperty(mockUrl))).willReturn(aResponse().withStatus(200).withBodyFile(mockJson)));
+        MocksHandler.stubUrlOptionsForCors(mockUrl);
+        MocksHandler.configureForLocalhost();
+        WireMock.stubFor(get(urlEqualTo(getProperty(mockUrl)))
+                .willReturn(aResponse().withStatus(200).withHeader("Access-Control-Allow-Origin", "*")
+                        .withHeader("Access-Control-Allow-Headers", "Content-Type")
+                        .withHeader("Access-Control-Allow-Methods", "GET, POST").withBodyFile(mockJson)));
     }
 }
