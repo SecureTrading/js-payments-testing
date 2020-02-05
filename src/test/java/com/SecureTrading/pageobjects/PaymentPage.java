87578740b4bcf95c8ee733d271e37599667cd205
James Simpson <james.simpson@securetrading.com>
2020-02-05 00:56:01 +0000

    Check if iframe is enabled before interacting with it

--- src/test/java/com/SecureTrading/pageobjects/PaymentPage.java
+++ src/test/java/com/SecureTrading/pageobjects/PaymentPage.java
@@ -171,7 +171,7 @@ public class PaymentPage extends BasePage {
 
     public void fillCreditCardInputField(FieldType fieldType, String value) throws InterruptedException {
         String frameName = fieldType.getIframeName();
-        waitUntilElementIsDisplayed(frameName, 20);
+        waitUntilElementIsDisplayed(By.id(frameName), 20);
         switchToIframe(frameName);
         switch (fieldType) {
             case CARD_NUMBER:
